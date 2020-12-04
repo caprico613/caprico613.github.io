f44606da98c82758a6bbfa144f4d5547b930ac34
侯永駿 <tom871226@gmail.com>
2020-12-04 13:03:43 +0800

    Update 3-4.js

--- Quizs/3-4/3-4.js
+++ Quizs/3-4/3-4.js
@@ -3,18 +3,16 @@ function up () {
 	var target = document.getElementById(buttonId);
 	var imgs = document.getElementsByTagName('img');
 
-	// reset
 	for (let i = 0; i < imgs.length; i++) {
-		imgs[i].style.opacity = '1';
-		imgs[i].style.top = '0px';
-	}
-
-
-	for (let i = 0; i < imgs.length; i++) {
-		if (imgs[i] !== target)
+		if (imgs[i] === target) {
+			imgs[i].style.opacity = '1';
+			imgs[i].style.top = '-20px';
+		}
+		else {
 			imgs[i].style.opacity = '0.5';
+			imgs[i].style.top = '0px';
+		}
 	}
-	target.style.top = '-20px';
 }
 
 window.onload = function () {
